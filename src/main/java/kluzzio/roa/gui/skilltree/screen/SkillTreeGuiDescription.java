18611e56cff48be8295c6c90640beb30dc6be3d2
Kluzzio <kluzzio.amp@gmail.com>
2022-03-11 00:45:18 -0800

    pain

--- src/main/java/kluzzio/roa/gui/skilltree/screen/SkillTreeGuiDescription.java
+++ src/main/java/kluzzio/roa/gui/skilltree/screen/SkillTreeGuiDescription.java
@@ -1,6 +1,7 @@
 package kluzzio.roa.gui.skilltree.screen;
 
 import io.github.cottonmc.cotton.gui.SyncedGuiDescription;
+import io.github.cottonmc.cotton.gui.client.BackgroundPainter;
 import io.github.cottonmc.cotton.gui.widget.WItemSlot;
 import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
 import io.github.cottonmc.cotton.gui.widget.WSprite;
@@ -17,15 +18,19 @@ import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.util.Identifier;
 
 public class SkillTreeGuiDescription extends SyncedGuiDescription {
-    private static final int INVENTORY_SIZE = 1;
+    private static final int INVENTORY_SIZE = 0;
 
     public SkillTreeGuiDescription(int syncId, PlayerInventory playerInventory, ScreenHandlerContext context) {
         super(RealmOfAlora.SCREEN_HANDLER_TYPE, syncId, playerInventory, getBlockInventory(context, INVENTORY_SIZE), getBlockPropertyDelegate(context));
 
         WPlainPanel root = new WPlainPanel();
         setRootPanel(root);
-        root.setSize(400, 500);
+        root.setSize(200, 300);
         root.setInsets(Insets.ROOT_PANEL);
+
+        WSprite backgroundTexture = new WSprite(new Identifier("roa:textures/client/gui/background.png"));
+        root.add(backgroundTexture, 0, 0, 200, 300);
+
 
         WItemSlot itemSlot = WItemSlot.of(blockInventory, 0);
         root.add(itemSlot, 4, 1);
